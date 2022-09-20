119fd0261d50b39ebad834181ddfdcb6af56beda
Esther Tsui <66723850+esther-tsui@users.noreply.github.com>
2022-09-20 15:53:30 +0100

    Initial Commit

---
+++ BMISwiftUI/BMISwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMISwiftUIApp.swift
+//  BMISwiftUI
+//
+//  Created by Wing Hei Tsui on 20/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct BMISwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
